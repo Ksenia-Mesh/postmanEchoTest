dac68cdc431107a2ec66c15f0eb2f5d28c3403f3
Ksenia <97454789+Ksenia-Mesh@users.noreply.github.com>
2023-03-29 21:13:51 +0300

    commit

--- src/test/java/ru/netology/test/postmanEchoTest.java
+++ src/test/java/ru/netology/test/postmanEchoTest.java
@@ -10,17 +10,13 @@ import org.junit.jupiter.api.Test;
 public class postmanEchoTest {
     @Test
     void shouldReturnDemoAccounts() {
-// Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
                 .when()
                 .post("/post")
-// Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(400)
                 .body("data", equalTo("some data"))
         ;
     }
